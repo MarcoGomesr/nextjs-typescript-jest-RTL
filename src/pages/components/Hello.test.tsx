f7ab27b9ff4df27453c09359c73a6004820ddb82
marco <marcogomesr@gmail.com>
2023-04-20 10:10:40 +0200

    initial project

---
+++ src/pages/components/Hello.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react"
+import Hello from "./Hello"
+
+it('should return the string "Hello World"', () => {
+  render(<Hello />)
+
+  const myElement = screen.getByText("Hello World")
+  expect(myElement).toBeInTheDocument()
+})
+
---
+++ src/pages/components/Hello.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <div>Hello World</div>
+  )
+}
+
+export default Hello
